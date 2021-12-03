5d67782167a0b68870ad1d63fd625cd1cce168de
Akashnani-star <dhanviakash1234@gmail.com>
2021-12-03 18:54:16 +0530

    folders restructured

--- reOrderingLinkedListsUsingRecursion/main.c
+++ reOrderingLinkedListsUsingRecursion/main.c
@@ -2,8 +2,8 @@
 #include "reOrder.h"
 
 int main(){
-	int a[] = {1,2,3,4,5,6,7};
-	Node* list = createLinkedList(a,7);
+	int a[] = {1,2,3,4,5};
+	Node* list = createLinkedList(a,5);
 	reOrder(list);
 	printLinkedList(list);
 }
